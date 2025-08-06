53c611369f5677800116bcb3b590ab7430b31f62
Harun UYSAL <harunuysalll@gmail.com>
2025-08-06 12:55:52 +0300

    Rolling Update and Level Configured

--- Assets/_Game Assets/Scripts/Player/PlayerAnimator.cs
+++ Assets/_Game Assets/Scripts/Player/PlayerAnimator.cs
@@ -14,6 +14,8 @@ public class PlayerAnimator : MonoBehaviour
     }
 
     private const float BallRadius = 0.5f;
+    private const float TurnSpeed = 720f;
+
     private Vector3 lastPosition;
 
     private void Update()
@@ -23,17 +25,26 @@ public class PlayerAnimator : MonoBehaviour
 
     private void UpdateRotation()
     {
-        var delta = lastPosition - transform.position;
-
+        var delta = transform.position - lastPosition;
         delta.y = 0;
 
         if (delta.sqrMagnitude < 0.0001f)
             return;
 
+
+        var targetDirection = delta.normalized;
+
+        if (targetDirection != Vector3.zero)
+        {
+            var targetRotation = Quaternion.LookRotation(targetDirection);
+            transform.rotation =
+                Quaternion.RotateTowards(transform.rotation, targetRotation, TurnSpeed * Time.deltaTime);
+        }
+
         var distance = delta.magnitude;
         var angle = distance / BallRadius * Mathf.Rad2Deg;
 
-        var rotationAxis = Vector3.Cross(delta.normalized, Vector3.up);
+        var rotationAxis = transform.right;
 
         ballTransform.Rotate(rotationAxis, angle, Space.World);
 
